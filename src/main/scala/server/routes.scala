111dd6fe16bd2ad5d57df378cc4235288b4a4ddb
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-05-16 18:26:26 +0300

    some strange

--- src/main/scala/server/routes.scala
+++ src/main/scala/server/routes.scala
@@ -5,6 +5,9 @@ import cats.effect.IO
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 
+import org.http4s.circe._
+import io.circe.syntax._
+
 object routes {
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
@@ -15,7 +18,7 @@ object routes {
       for {
         v        <- value.as[String]
         _        <- IO(println(s"DEBUG ->>>>> $v"))
-        response <- Ok(value.as[String])
+        response <- Ok("println(m.asJson.toString)\n  println(\"----------------------\")".asJson)
       } yield response
 
   }
