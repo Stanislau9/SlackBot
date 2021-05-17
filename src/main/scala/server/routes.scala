32d86d5118192afa4cf0ea860a3a4e9ab42f2774
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-05-17 10:25:23 +0300

    some strange

--- src/main/scala/server/routes.scala
+++ src/main/scala/server/routes.scala
@@ -2,13 +2,14 @@ package server
 
 import cats.effect.IO
 import io.circe.Json
+import io.circe.generic.JsonCodec
 import io.circe.syntax.EncoderOps
 import org.http4s.HttpRoutes
 import org.http4s.circe.jsonDecoder
 import org.http4s.dsl.io._
 
 object routes {
-
+  @JsonCodec case class triggerId(trigger_id: String)
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
     case GET -> Root / value => Ok(value)
@@ -16,7 +17,7 @@ object routes {
     case value @ POST -> Root =>
       for {
         v        <- value.as[String].map(_.drop(8).asJson)
-        _        <- IO(println(s"DEBUG ->>>>> ${v}"))
+        _        <- IO(println(s"DEBUG ->>>>> ${v.as[triggerId]}"))
         response <- Ok(value.as[String])
       } yield response
 
