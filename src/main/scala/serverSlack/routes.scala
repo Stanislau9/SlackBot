b6d68ca2e15bb5a3d7f10bee356c742adda48473
Stanislau9 <70797922+Stanislau9@users.noreply.github.com>
2021-05-17 12:44:51 +0300

    some strange

--- src/main/scala/serverSlack/routes.scala
+++ src/main/scala/serverSlack/routes.scala
@@ -1,19 +1,15 @@
 package serverSlack
 
 import cats.effect.IO
-import io.circe.Decoder
-import io.circe.generic.semiauto.deriveDecoder
-//import io.circe.syntax.EncoderOps
-import org.http4s.{EntityDecoder, HttpRoutes}
+
+import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
-import org.http4s.circe.jsonOf
+
 import org.http4s.dsl.io._
 import serverSlack.slackApiMethods.chat._
+import io.circe.syntax._
 
 object routes {
-
-  implicit val authorDecoder: Decoder[triggerId] = deriveDecoder
-  //implicit val authorDecoderEntity: EntityDecoder[IO, postMessage] = jsonOf
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
 
@@ -21,8 +17,8 @@ object routes {
 
     case value @ POST -> Root =>
       for {
-        v        <- value.as[triggerId]
-        _        <- IO(println(s"DEBUG ->>>>> $v"))
+        v        <- value.as[String]
+        _        <- IO(println(s"DEBUG ->>>>> ${v.drop(8).asJson.as[triggerId]}"))
         response <- Ok(value.as[String])
       } yield response
 
